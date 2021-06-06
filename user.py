b969f0f6770a318a1516ff47604208e1f8bb17bc
AmritSapkota <mrtspkt@gmail.com>
2021-06-06 18:25:50 +0900

    commit

--- user.py
+++ user.py
@@ -1,20 +1,112 @@
-class User:
-    def get_user_info():
-        name = input("Name: ")
-        phoneNo = input("Contact number: ")
-        return name, phoneNo
+import psycopg2
+import random
 
-    def search_route():
-        print("Please enter bus number: ")
+def connect():
+    try:
+        conn = psycopg2.connect(database="pythonproject", user="postgres",
+                                password="amrit", host="127.0.0.1", port="5432")
+        cur = conn.cursor()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error while creating PostgreSQL table", error)
+    return conn, cur
 
-    def reserve_ticket():
-        print("Please enter bus number: ")
 
-    def cancel_ticket():
-        print("Please enter ticket number: ")
 
-    def reserve_vehicle():
-        print("Please enter ")
-        
-    def time_table():
+def menu():
+    print("**********Services************")
+    print("1. Search Route")
+    print("2. Reserve Ticket")
+    print("3. Cancel Ticket")
+    print("4. Reserve vehicle")
+    print("5. Vehicle Timetable")
+
+    selectedService = int(input("Select service: "))
+    if (selectedService == 1):
         pass
+    elif (selectedService == 2):
+        reserve_ticket()
+    elif (selectedService == 3):
+        pass
+    elif (selectedService == 4):
+        pass
+    elif (selectedService == 5):
+        pass
+    else:
+        print("-------Please Enter Vaild Service.-----------")
+        menu()
+
+
+def searchRoute():
+    route = input("Enter your destination: ")
+
+    #fetching all bus info with this destination
+
+def create_table():
+    conn, cur = connect()
+    try:
+        cur.execute(
+            'CREATE TABLE ticket (name VARCHAR(10), dest VARCHAR(10),  busno VARCHAR(10), phoneno INT, ticketno INT)')
+    except:
+        print('Error while creating table.')
+    conn.commit()
+
+def fetch_data_ticket():
+    conn, cur = connect()
+    try:
+        cur.execute('SELECT * FROM ticket')
+    except:
+        print('Ticket Error')
+    data = cur.fetchall()
+    return data
+
+def check_ticket(ticketno):
+    data = fetch_data_ticket()
+    flag = True
+    for row in data:
+        if row[4] == ticketno:
+            flag = False
+            break
+        else:
+            continue
+
+    return flag
+def generate_rand():
+    return random.randint(1000101, 9999999)
+
+def reserve_ticket():
+     #create_table()
+     name = input("Name: ")
+     dest = input("Destinatino: ")
+     busno = input("Bus no: ")
+     phoneno = int(input("Phone no: "))
+     ticket_no = random.randint(1000101, 9999999)
+
+     #checking ticket
+     while not check_ticket(ticket_no):
+         ticket_no = generate_rand()
+
+     conn, cur = connect()
+     try:
+        cur.execute('INSERT INTO ticket VALUES(%s, %s, %s, %s, %s)',
+                        (name, dest, busno, phoneno, ticket_no))
+        print("!! Ticket Reserved !!")
+
+     except Exception as e:
+            print('error', e)
+    # commiting the transaction.
+     conn.commit()
+
+
+
+
+
+def cancel_ticket():
+    pass
+
+
+def reserve_vehicle():
+    pass
+
+
+def vehicle_timetable():
+    pass
